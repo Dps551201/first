7d31f6dca917880bffde26a4f480c3032177bdae
ZSA <m555pa@mail.ru>
2019-02-04 13:15:26 +0600

    изменение записи на странице

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Загаров Сергей - Первый проект
                 </div>
 
                 <div class="links">
