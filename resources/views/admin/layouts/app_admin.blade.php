d7d958b0383d09f34d1c065b92525027e21d2a4b
ZSA <m555pa@mail.ru>
2019-02-07 23:50:46 +0600

    article4

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -85,5 +85,9 @@
         @yield('content')
     </main>
 </div>
+
+<!-- Scripts -->
+<script src="{{ asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
